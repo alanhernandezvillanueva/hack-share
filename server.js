3e2fc840da52a9330d65ec405aa90b79ca3af203
alanhernandezvillanueva <79774478+alanhernandezvillanueva@users.noreply.github.com>
2021-12-31 14:13:59 -0800

    added routes to create a delete posts

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.set("view engine", "handlebars");
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
