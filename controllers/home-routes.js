2a25d3dfeafa508ac24384a66adee19cc659d8e3
Nick Rizzo <nickrizzo77277@gmail.com>
2022-01-02 11:55:22 -0600

    commit to pull

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,19 +1,19 @@
 const router = require("express").Router();
 
 router.get("/login", (req, res) => {
-  //render your handlebar file insidet he {{{body}}}
+  //render your handlebar file inside the {{{body}}}
   res.render("login");
 });
 router.get("/signup", (req, res) => {
-  //render your handlebar file insidet he {{{body}}}
+  //render your handlebar file inside the {{{body}}}
   res.render("signup");
 });
 router.get("/", (req, res) => {
-  //render your handlebar file insidet he {{{body}}}
+  //render your handlebar file inside the {{{body}}}
   res.render("homepage");
 });
 router.get("/search", (req, res) => {
-  //render your handlebar file insidet he {{{body}}}
+  //render your handlebar file inside the {{{body}}}
   res.render("search");
 });
 
