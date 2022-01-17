6615aaca569359a0a38c157170da941dc0a3a6f9
alanhernandezvillanueva <79774478+alanhernandezvillanueva@users.noreply.github.com>
2022-01-16 19:11:04 -0800

    commit

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // connect to data base 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = new Sequelize(process.env.JAWSDB_URL, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
